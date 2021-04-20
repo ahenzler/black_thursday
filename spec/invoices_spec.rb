87b074ab243ad04d6ab6a1b4e9ec58022261e981
ahenzler <aliciahenzler@outlook.com>
2021-04-20 17:05:36 -0600

    fix time

--- spec/invoices_spec.rb
+++ spec/invoices_spec.rb
@@ -36,11 +36,11 @@ RSpec.describe Invoice do
     end
 
     it 'can return time created at' do
-      expect(invoice_repo.invoice_list[0].created_at).to eq(Time.parse("2009-02-07 00:00:00 -0500"))
+      expect(invoice_repo.invoice_list[0].created_at).to eq(Time.parse("2009-02-07 00:00:00 -0700"))
     end
 
     it 'can return time updated at' do
-      expect(invoice_repo.invoice_list[0].updated_at).to eq(Time.parse("2014-03-15 00:00:00 -0400"))
+      expect(invoice_repo.invoice_list[0].updated_at).to eq(Time.parse("2014-03-15 00:00:00 -0600"))
     end
   end
 end
